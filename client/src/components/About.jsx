b5d29cdacd38877258cd88af950df747e516d5eb
Em - Desktop <jekahn@clemson.edu>
2025-09-12 17:39:07 -0400

    Apparently nothing I modify in tinkering works, it all stays the same and I do not understand why what the hell is any of this AAAAAAAAAAAAAAAAAAAAAAAAA

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom';
 export default function About() {
   return (
     <div>
+      <h1>I swear to god if this doesn't work I will scream.</h1>
       <h1>This will be the about page</h1>
       <Link to="/login">Go to Login Page</Link>
+      <h1>Below is a test of whether the addition of booststrap did fuck all.</h1>
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
   );
 }
