8658afef5abb050cbcf797410304692a0edc9af7
Em - Desktop <jekahn@clemson.edu>
2025-09-12 18:09:19 -0400

    Reverted desperation changes, added spinner.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Login() {
   return (
     <div>
-      <h1>This will be the login page - Does changing this do LITERALLY ANYTHING AT ALL????</h1>
+      <h1>This will be the login page</h1>
       <Link to="/about">Go to About Page</Link>
     </div>
   );
