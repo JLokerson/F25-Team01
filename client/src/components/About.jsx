6eb6683b222eb07e1d1fe1d9bc699922396b50da
Em - Desktop <jekahn@clemson.edu>
2025-09-12 17:52:15 -0400

    still no worky

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function About() {
   return (
     <div>
-      <h1>I swear to god if this doesn't work I will scream.</h1>
+      <h1>I swear to god if this doesn\'t work I will scream.</h1>
       <h1>This will be the about page</h1>
       <Link to="/login">Go to Login Page</Link>
       <h1>Below is a test of whether the addition of booststrap did fuck all.</h1>
