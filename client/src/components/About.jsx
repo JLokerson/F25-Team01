8916a8375b42adbf9d3010b14abdba00de285d07
Emerson Kahn <jekahn@clemson.edu>
2025-09-15 12:33:13 -0400

    Added a TOS display button

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
+  function DisplayTOS() {
+    // Mock login logic
+    alert(`By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`);
+  };
+
 export default function About() {
   return (
     <div>
@@ -10,6 +15,8 @@ export default function About() {
       <div className="spinner-border text-primary" role="status">
         <span className="sr-only"> </span>
       </div>
+      <br></br>
+      <button type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
     </div>
   );
 }
