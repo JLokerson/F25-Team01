1cd9a6e8e177eefe88c2f0fd7b45d37d5d5e9cbb
Vanishing42 <113644531+Vanishing42@users.noreply.github.com>
2025-09-15 15:22:45 -0400

    Tos is now handled using a useState and an entirely unique element rather than an alert.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-
-function DisplayTOS() {
-  // Mock login logic
-  alert(`By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`);
-};
+import { useState } from 'react';
 
 
-export default function About() {
+
+
+
+function MostOfTheText(){
   return (
-    <div>
-      <div style={{minHeight:"92vh",maxHeight:"92vh"}}>
+      <div style={{minHeight:"88vh",maxHeight:"88vh"}}>
         <h1>This will be the about page</h1>
         <Link to="/login">Go to Login Page</Link>
         <br></br>
@@ -19,7 +17,32 @@ export default function About() {
           <span className="sr-only"> </span>
         </div>
       </div>
-      <button style={{minHeight:"8vh",maxHeight:"8vh"}} type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
-    </div>
   );
+}
+
+export default function About() {
+  const [ShowTOS, setShowTOS] = useState('');
+  function DisplayTOS() {
+  setShowTOS(!ShowTOS);
+  };
+
+  if(ShowTOS){
+    return (
+    <div>
+      {MostOfTheText()}
+      <div style={{minHeight:"12vh",maxHeight:"12vh"}}>
+        <p>By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`</p>
+        <button style={{minHeight:"8vh",maxHeight:"8vh"}} type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
+      </div>
+    </div>);
+  }else{
+    return (
+    <div>
+      {MostOfTheText()}
+      <div style={{minHeight:"12vh",maxHeight:"12vh"}}>
+        <p style={{color:"#ffffff"}}>This text doesn't exist</p>
+        <button style={{minHeight:"8vh",maxHeight:"8vh",}} type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
+      </div>
+    </div>);
+  }
 }
