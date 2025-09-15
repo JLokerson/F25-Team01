f50ed33d0040b8a71a9cb18f0f1b235d29d2a7ca
Emerson Kahn <jekahn@clemson.edu>
2025-09-15 12:45:37 -0400

    Always will be at the bottom, too big though. Unsure where space is going.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
-  function DisplayTOS() {
-    // Mock login logic
-    alert(`By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`);
-  };
+function DisplayTOS() {
+  // Mock login logic
+  alert(`By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`);
+};
+
 
 export default function About() {
   return (
     <div>
-      <h1>This will be the about page</h1>
-      <Link to="/login">Go to Login Page</Link>
-      <div className="spinner-border text-primary" role="status">
-        <span className="sr-only"> </span>
+      <div style={{minHeight:"75vw",maxHeight:"75vw"}}>
+        <h1>This will be the about page</h1>
+        <Link to="/login">Go to Login Page</Link>
+        <div className="spinner-border text-primary" role="status">
+          <span className="sr-only"> </span>
+        </div>
       </div>
-      <br></br>
-      <button type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
+      <button style={{minHeight:"25vw",maxHeight:"25vw"}} type="submit" onClick={DisplayTOS} className="btn btn-info">TOS</button>
     </div>
   );
 }
