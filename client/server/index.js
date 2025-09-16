bcbcc57cb3cb7d32ab71eda9fb167f84c59e6101
ohmy8 <128621820+ohmy8@users.noreply.github.com>
2025-09-16 15:46:00 -0400

    Port change for server
    5000 to 4000

--- client/server/index.js
+++ client/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 5000; // Can change the port if needed
+const port = 4000; // Can change the port if needed
 
 app.use(cors()); // Enable CORS for cross-origin requests
 app.use(express.json()); // Enable parsing JSON request bodies
