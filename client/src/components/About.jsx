d3653af2faf4ac561782632b1e7ae83a63b3faa4
Emerson Kahn <jekahn@clemson.edu>
2025-09-16 14:35:13 -0400

    Last minute fix

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 
 
 
+function MostOfTheText(InputBool){
+  let navigate = useNavigate(); 
+const GoLogin = () =>{ 
+  let path = `/login`; 
+  if(InputBool){
+    navigate(path);
+  }else{
+    alert('Must view TOS before login');
+  }
+}
 
-function MostOfTheText(){
   return (
       <div style={{minHeight:"88vh",maxHeight:"88vh"}}>
         <h1>This will be the about page</h1>
-        <Link to="/login">Go to Login Page</Link>
+        <br></br>
+        <button type="submit" onClick={GoLogin} className="btn btn-info">To Login</button>
         <br></br>
         <div className="spinner-border text-primary" role="status">
           <span className="sr-only"> </span>
@@ -30,7 +40,7 @@ export default function About() {
   if(ShowTOS){
     return (
     <div>
-      {MostOfTheText()}
+      {MostOfTheText(ShowTOS)}
       <div style={{minHeight:"12vh",maxHeight:"12vh"}}>
         <p>By using our site you hereby agree that we, the designers and hosts of this service, are not to be held liable for any and all harm, fiscal or otherwise, that may arise from the use of our software. You agree that should you have any legal dispute with us, it is to be handled via arbitration in Clemson, South Carolina. The arbitrator in any such arrangement will be selected and/or approved by us, Network Drivers.`</p>
         <button style={{minHeight:"8vh",maxHeight:"8vh"}} type="submit" onClick={DisplayTOS} className="btn btn-info">Hide TOS</button>
@@ -39,7 +49,7 @@ export default function About() {
   }else{
     return (
     <div>
-      {MostOfTheText()}
+      {MostOfTheText(ShowTOS)}
       <div style={{minHeight:"12vh",maxHeight:"12vh"}}>
         <p style={{color:"#ffffff"}}>This text doesn't exist</p>
         <button style={{minHeight:"8vh",maxHeight:"8vh",}} type="submit" onClick={DisplayTOS} className="btn btn-info">Show TOS</button>
