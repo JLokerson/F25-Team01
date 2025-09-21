6a63d29b71b13298918c6e69c4636918c8258216
Vanishing42 <emersonkahn@gmail.com>
2025-09-20 20:31:10 -0400

    Minor edits to the navbars
    Home button now actually takes you to the home area of a given type of user. Not sure what we want to put in the home areas aside from navbars to reach other things but we have them now,

--- client/src/components/SponsorNavbar.jsx
+++ client/src/components/SponsorNavbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function SponsorNavbar() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Sponsor Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -13,7 +13,7 @@ export default function SponsorNavbar() {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/SponsorHome">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
