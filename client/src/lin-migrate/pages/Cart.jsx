d4367a60d105d89ca49add82ab351ebbf50497c1
Jason L <Jas0n17L1n@gmail.com>
2025-09-22 17:38:44 -0400

    (insert) populated linmigrate/{components,page,styles}

---
+++ client/src/lin-migrate/pages/Cart.jsx
@@ -0,0 +1,8 @@
+export default function Cart() {
+  return (
+    <div className='container'>
+      <h2 className='text-2xl font-bold mb-4'>Shopping Cart</h2>
+      <p>(Cart state handling to be added here)</p>
+    </div>
+  )
+}
