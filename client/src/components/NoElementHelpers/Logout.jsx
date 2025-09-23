7d05dc95a77cf840a2130dd6d37135a142bb3935
Vanishing42 <phantomsea4@gmail.com>
2025-09-23 14:23:17 -0400

    Create Logout.jsx
    Added logout function, with a placeholder comment to clear the login info once we have state information to track it with.

---
+++ client/src/components/NoElementHelpers/Logout.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+export default function Logout(){
+    // Set all state variables to null/clear them
+
+    navigate('/about');
+}
