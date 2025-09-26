b5867b0f8cd58ead75361abcb71e4c360dbfe78a
Phantomcreator42 <emersonkahn@gmail.com>
2025-09-26 10:25:31 -0400

    Implemented SHA256 hashing operation as helper method for passwords
    Functionality of hashing operation implemented.

---
+++ client/src/components/MiscellaneousParts/HashPass.jsx
@@ -0,0 +1,16 @@
+async function digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+    const hashBuffer = await window.crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join(""); // convert bytes to hex string
+return hashHex;
+}
+
+
+
+
+export default async function HashPassword(passwd,salt){
+    return await digestMessage(await digestMessage(passwd + salt));
+}
