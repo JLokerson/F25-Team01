8f7d958fa1477214e8df56a824dc7fba01c2d015
Jason L <Jas0n17L1n@gmail.com>
2025-09-27 16:22:42 -0400

    (modify) added DriverHome stuff

--- client/src/components/DriverHome.jsx
+++ client/src/components/DriverHome.jsx
@@ -8,6 +8,11 @@ export default function DriverHome() {
         <div>
             {DriverNavbar()}
             {/* Place driver home page text below here */}
+            console.log("Driver home page rendered. DriverHome.jsx");
+            <div className="container my-5">
+                <h1>Welcome to the Driver Home Page</h1>
+                <p>This is where drivers can see their dashboard and relevant information.</p>
+            </div>
         </div>
     );
 }
