7c136faf39c5186e8e12e8d90368a1f13d973ef8
Jason L <Jas0n17L1n@gmail.com>
2025-09-27 15:59:50 -0400

    (moved) moved Home.css to its own styles page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
+import '../styles/Home.css';
 
 export default function Home() {
   const navigate = useNavigate();
