d8d58017c2d623a1cb41207734897613c5ec26ac
Jason L <Jas0n17L1n@gmail.com>
2025-09-30 14:51:22 -0400

    (delete) cartPage.jsx

--- client/src/components/CartPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-export default function CartPage(){
-  return (
-    <div className="container my-5">
-      <h3>Your Cart</h3>
-    </div>
-  );
-}
