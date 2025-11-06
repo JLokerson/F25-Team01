dc8f73291f7bdbec93a52e0deb0eed8d29c32151
Phantomcreator42 <emersonkahn@gmail.com>
2025-11-06 14:59:16 -0500

    Update ReportView.jsx
    Tie it to servercall method, hopefully this allows full retrieval to read from that and display.
    I hope.

--- client/src/components/MiscellaneousParts/ReportView.jsx
+++ client/src/components/MiscellaneousParts/ReportView.jsx
@@ -1,28 +1,22 @@
 import './App.css';
 import React, {useState} from react;
+import {getAllAuditRecords} from './ServerCall.jsx';
 
 // Filter is an untyped var on purpose, intended to be passed as a bunch of possible filters
 // we can apply to the data after initial retrieval, though none have yet been implemented.
 function ReportView(Filter) {
-    const listItems = chemists.map(person =>
+  returned = getAllAuditRecords();
+  const listItems = returned.map(person =>
     <li>
-        <img src={getImageUrl(person)} alt={person.name}/>
-        <p>
-            <b>{person.name}:</b>
-            {' ' + person.profession + ' '}
-            known for {person.accomplishment}
-        </p>
+      <img src={getImageUrl(person)} alt={person.name}/>
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
     </li>
-);
-  return (
-    <div className="App">
-        <form>
-          <h1>React File Upload</h1>
-          <input type="file" onChange={handleChange}/>
-          <button type="submit">Upload</button>
-        </form>
-    </div>
   );
+  return listItems;
 }
 
 export default ReportView;
