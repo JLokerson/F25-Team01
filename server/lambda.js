c9f87219c56b0c5175b0bd6d30bbf35610e11284
Jason L <Jas0n17L1n@gmail.com>
2025-11-08 12:11:07 -0500

    Safety commit

--- server/lambda.js
+++ server/lambda.js
@@ -1,4 +1,4 @@
-//import for aws lambda
-const serverless = require('serverless-http');
+// Import for AWS Lambda
+const serverless = require("serverless-http");
 const app = require("./index");
 module.exports.handler = serverless(app);
