7a00123f05ce44bee9c9b4a6114c4771d672f875
Phantomcreator42 <emersonkahn@gmail.com>
2025-11-23 14:36:52 -0500

    Should fix admin and sponsor variants. Pushing to then pull on other pc for testing

--- client/src/components/ProfilePages/SponsorProfile.jsx
+++ client/src/components/ProfilePages/SponsorProfile.jsx
@@ -12,6 +12,8 @@ export default function SponsorProfile() {
     const [showPasswordChangeButton, setShowPasswordChangeButton] = useState(false);
     const [sponsorInfo, setSponsorInfo] = useState(null);
     const [sponsorName, setSponsorName] = useState(null);
+    const [userID, setUserID] = useState([]);
+    
     
     // Ensure this is actually a sponsor user.
     // TO-DO: verify the login returned success and not fail, rn only checks if error on retrieval.
@@ -100,6 +102,7 @@ export default function SponsorProfile() {
 
     const fetchSponsorInfo = async () => {
         const userInfo = getUserInfo();
+        setUserID(userInfo.UserID);
         console.log('SponsorProfile - UserInfo:', userInfo); // Debug log
         if (userInfo && userInfo.UserID) {
             try {
@@ -244,7 +247,7 @@ export default function SponsorProfile() {
                     </div>
                 )}
 
-                {HelperPasswordChange(sponsor.userid)}
+                {HelperPasswordChange(userID)}
             </div>
         );
     }else{
