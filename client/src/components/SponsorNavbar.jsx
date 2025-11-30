b0939c89a86d1b2646c94330386f33edf36a4801
JLokerson <jlokers@clemson.edu>
2025-11-30 12:15:52 -0500

    moved bulk loading to driver management and fixed product styling reversion

--- client/src/components/SponsorNavbar.jsx
+++ client/src/components/SponsorNavbar.jsx
@@ -93,9 +93,6 @@ export default function SponsorNavbar() {
                             <li className="nav-item">
                                 <Link className="nav-link" to="/sponsorauditview">Audit</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/sponsorbulkupload">Bulk Upload</Link>
-                            </li>
                         </ul>
                         <div className="d-flex align-items-center">
                             {isAdminImpostor && (
